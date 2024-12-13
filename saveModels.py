d8a985912f1c92784b5a35f076f4bca4e2f5bc6b
leolipa <leonardo.liparulo@mail.polimi.it>
2024-12-13 10:10:42 +0100

    evaluator problem

--- saveModels.py
+++ saveModels.py
@@ -6,7 +6,12 @@ from ModelController import ModelController
 import pandas as pd
 from Evaluation.Evaluator import EvaluatorHoldout
 from Data_manager.split_functions.split_train_validation_random_holdout import split_train_in_two_percentage_global_sample
+from Recommenders.GraphBased.RP3betaRecommender import RP3betaRecommender
 from Recommenders.KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
+from Recommenders.KNN.ItemKNN_CFCBF_Hybrid_Recommender import ItemKNN_CFCBF_Hybrid_Recommender
+from Recommenders.KNN.UserKNNCFRecommender import UserKNNCFRecommender
+from Recommenders.NonPersonalizedRecommender import TopPop
+from Recommenders.SLIM.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
 from Recommenders.SLIM.SLIMElasticNetRecommender import SLIMElasticNetRecommender
 
 
@@ -24,24 +29,16 @@ ICM = pd.read_csv(filepath_or_buffer="Data/data_ICM_metadata.csv",
 URM_all, ICM_all = DataHandler.create_urm_icm(URM_all_dataframe, ICM)
 
 controller = ModelController(URM_all, ICM_all)
-evaluator_validation = EvaluatorHoldout(controller.URM_test, cutoff_list=[10])
+evaluator_test = EvaluatorHoldout(controller.URM_test, cutoff_list=[10])
+
 
 URM_all, ICM_all = DataHandler.create_urm_icm(URM_all_dataframe, ICM)
 
-controller = ModelController(URM_all, ICM_all)
-
-'''itemKnnBoost = ItemKNNCFRecommender(controller.URM_train_boost)
-itemKnnBoost.fit(topK= 10, shrink= 0, similarity= 'cosine')
-itemKnnBoost.save_model(folder_path="_saved_models", file_name = "ItemKNNCFBoost")
-itemknnTrain = ItemKNNCFRecommender(controller.URM_train)
-itemKnnBoost.fit(topK= 10, shrink= 0, similarity= 'cosine')
-itemKnnBoost.save_model(folder_path="_saved_models", file_name = "ItemKNNCFTrain")
-itemknnAll = ItemKNNCFRecommender(controller.URM_all)
-itemKnnBoost.fit(topK= 10, shrink= 0, similarity= 'cosine')
-itemKnnBoost.save_model(folder_path="_saved_models", file_name = "ItemKNNCFAll")'''
-itemknnTrain = ItemKNNCFRecommender(controller.URM_train)
-itemknnTrain.load_model(folder_path="_saved_models", file_name="ItemKNNCFTrain")
-result_df, _ = evaluator_validation.evaluateRecommender(itemknnTrain)
-print(result_df.loc[10]["MAP"])
-res,_ = controller.evaluator_test.evaluateRecommender(itemknnTrain)
+hyb = ItemKNN_CFCBF_Hybrid_Recommender(controller.URM_train, controller.ICM_all)
+hyb.load_model(folder_path="_saved_models", file_name="ItemKNN_CFCBF_Hybrid_Recommender_train")
+res,_ = controller.evaluator_test.evaluateRecommender(hyb)
 print (res.loc[10]["MAP"])
+rr,_ = evaluator_test.evaluateRecommender(hyb)
+print(rr.loc[10]["MAP"])
+
+
