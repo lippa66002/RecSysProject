ee78ab077e15d3a809ebfecd647d07eef614c894
leolipa <leonardo.liparulo@mail.polimi.it>
2024-12-22 17:00:30 +0100

    easer testing

--- trainEase.py
+++ trainEase.py
@@ -1,9 +1,10 @@
-import pandas as pd
 
 import DataHandler
-from ItemKnnCfClusterized import URM_train
-from ModelController import ModelController
 from Recommenders.EASE_R.EASE_R_Recommender import EASE_R_Recommender
+
+from ModelController import ModelController
+import pandas as pd
+
 
 URM_all_dataframe = pd.read_csv(filepath_or_buffer="Data/data_train.csv",
                                 sep=",",
@@ -19,7 +20,7 @@ URM_all, ICM_all = DataHandler.create_urm_icm(URM_all_dataframe, ICM)
 
 controller = ModelController(URM_all, ICM_all)
 
-ease = EASE_R_Recommender(URM_train)
+ease = EASE_R_Recommender(controller.URM_train)
 ease.fit()
 dd, _ = controller.evaluator_test.evaluateRecommender(ease)
 print(dd.loc[10]["MAP"])
