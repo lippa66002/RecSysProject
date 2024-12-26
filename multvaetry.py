485bade28987e7e2b1b80068bbec458a833c0af7
leolipa <leonardo.liparulo@mail.polimi.it>
2024-12-26 17:14:34 +0100

    multvae try

--- multvaetry.py
+++ multvaetry.py
@@ -1,14 +1,9 @@
-import optuna
+import pandas as pd
 
 import DataHandler
 from ModelController import ModelController
-import pandas as pd
-from ModelNames import ModelName
-
-
-print("Start")
-from Recommenders.EASE_R.EASE_R_Recommender import EASE_R_Recommender
-
+from Recommenders.Neural.MultVAERecommender import MultVAERecommender
+from Recommenders.Neural.MultVAE_PyTorch_Recommender import MultVAERecommender_PyTorch
 
 URM_all_dataframe = pd.read_csv(filepath_or_buffer="Data/data_train.csv",
                                 sep=",",
@@ -21,11 +16,9 @@ ICM = pd.read_csv(filepath_or_buffer="Data/data_ICM_metadata.csv",
                                 dtype={0:int, 1:int, 2:float},
                                 engine='python')
 URM_all, ICM_all = DataHandler.create_urm_icm(URM_all_dataframe, ICM)
-
 controller = ModelController(URM_all, ICM_all)
 
-
-params = controller.optunizer(ModelName.ScoresHybridRecommender)
-
-
-
+mult = MultVAERecommender_PyTorch(controller.URM_train, controller.ICM_all)
+mult.fit()
+cc,_ = controller.evaluator_test.evaluateRecommender(mult)
+print(cc.loc[10]["MAP"])
