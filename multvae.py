6a48c53de9cb7297b62a43a4a0f368ec8b469e9f
leolipa <leonardo.liparulo@mail.polimi.it>
2024-12-26 19:55:47 +0100

    try make easer training faster

--- multvae.py
+++ multvae.py
@@ -3,14 +3,10 @@ import pandas as pd
 import scipy.sparse as sps
 import zipfile
 
-from Data_manager.split_functions.split_train_validation_random_holdout import \
-    split_train_in_two_percentage_global_sample
+
 from Evaluation.Evaluator import EvaluatorHoldout
 from Optimize.SaveResults import SaveResults
-from Recommenders.GraphBased.RP3betaRecommender import RP3betaRecommender
-from Recommenders.KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
-from Recommenders.KNN.ItemKNN_CFCBF_Hybrid_Recommender import ItemKNN_CFCBF_Hybrid_Recommender
-from Recommenders.KNN.UserKNNCFRecommender import UserKNNCFRecommender
+
 from Recommenders.Neural.MultVAERecommender import MultVAERecommender
 from Recommenders.SLIM.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
 from Recommenders.SLIM.SLIMElasticNetRecommender import SLIMElasticNetRecommender
@@ -41,14 +37,14 @@ def objective_function_multVAE(optuna_trial):
         "learning_rate": optuna_trial.suggest_float("learning_rate", 1e-4, 1e-2, log=True),
         "batch_size": optuna_trial.suggest_int("batch_size", 32, 512),
         "dropout": optuna_trial.suggest_float("dropout", 0.1, 0.5),
-        "total_anneal_steps": optuna_trial.suggest_int("total_anneal_steps", 10000, 200000),
         "anneal_cap": optuna_trial.suggest_float("anneal_cap", 0.1, 0.5),
-        "p_dims": [URM_trainval.shape[1], URM_trainval.shape[1]],
         "epochs" : optuna_trial.suggest_int("epochs" ,50,300)
     }
     recommender_instance.fit(**full_hyperp)
     result_df, _ = evaluator_test.evaluateRecommender(recommender_instance)
     return result_df.loc[10]["MAP"]
+
+
 optuna_study = optuna.create_study(direction="maximize")
 save_results = SaveResults()
 optuna_study.optimize(objective_function_multVAE,
