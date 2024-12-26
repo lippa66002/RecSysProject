57516a0667befb392a7b83a78e3966f8309cf311
leolipa <leonardo.liparulo@mail.polimi.it>
2024-12-26 18:09:05 +0100

    multvae try

--- multvae.py
+++ multvae.py
@@ -35,3 +35,27 @@ mult = MultVAERecommender(URM_trainval)
 mult.fit()
 dd, _ = evaluator_test.evaluateRecommender(mult)
 print(dd.loc[10]["MAP"])
+
+
+def objective_function_multVAE(optuna_trial):
+    recommender_instance = MultVAERecommender(URM_trainval)
+    full_hyperp = {
+        # "epochs": optuna_trial.suggest_int("epochs", 10, 100),
+        "learning_rate": optuna_trial.suggest_float("learning_rate", 1e-4, 1e-2, log=True),
+        "batch_size": optuna_trial.suggest_int("batch_size", 32, 512),
+        "dropout": optuna_trial.suggest_float("dropout", 0.1, 0.5),
+        "total_anneal_steps": optuna_trial.suggest_int("total_anneal_steps", 10000, 200000),
+        "anneal_cap": optuna_trial.suggest_float("anneal_cap", 0.1, 0.5),
+        "p_dims": [URM_trainval.shape[1], URM_trainval.shape[1]],
+        "epochs" : optuna_trial.suggest_int("epochs" ,50,300)
+    }
+    recommender_instance.fit(**full_hyperp)
+    result_df, _ = evaluator_test.evaluateRecommender(recommender_instance)
+    return result_df.loc[10]["MAP"]
+optuna_study = optuna.create_study(direction="maximize")
+save_results = SaveResults()
+optuna_study.optimize(objective_function_multVAE,
+                              callbacks=[save_results],
+                              n_trials=50)
+print(save_results.results_df)
+print(optuna_study.best_trial.params)
