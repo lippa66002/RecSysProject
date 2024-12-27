21a32f33bde3c08943aef32a6c6de988c4604b90
leolipa <leonardo.liparulo@mail.polimi.it>
2024-12-27 12:21:43 +0100

    .

--- trainslim.py
+++ trainslim.py
@@ -1,8 +1,7 @@
 import os
 import sys
 
-# Aggiungi la directory principale al percorso dei moduli
-sys.path.append(os.path.abspath(".."))
+
 
 from ModelController import ModelController
 from ModelNames import ModelName
