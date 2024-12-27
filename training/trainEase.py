5cdd2c97388d63d28e2eaee18cb0da58dc4ba70b
leolipa <leonardo.liparulo@mail.polimi.it>
2024-12-27 11:15:41 +0100

    another update of pytorch version

--- training/trainEase.py
+++ training/trainEase.py
@@ -1,5 +1,5 @@
 
-import DataHandler
+
 from EASE_R_Recommender import EASE_R_Recommender
 from EASE__R_PyTorch import EASE_R_PyTorch
 from ModelNames import ModelName
@@ -9,19 +9,9 @@ from ModelController import ModelController
 import pandas as pd
 
 
-URM_all_dataframe = pd.read_csv(filepath_or_buffer="../Data/data_train.csv",
-                                sep=",",
-                                dtype={0:int, 1:int, 2:float},
-                                engine='python')
-users = pd.read_csv(filepath_or_buffer="../Data/data_target_users_test.csv")
 
-ICM = pd.read_csv(filepath_or_buffer="../Data/data_ICM_metadata.csv",
-                  sep=",",
-                  dtype={0:int, 1:int, 2:float},
-                  engine='python')
-URM_all, ICM_all = DataHandler.create_urm_icm(URM_all_dataframe, ICM)
 
-controller = ModelController(URM_all, ICM_all)
+controller = ModelController()
 
 easer = EASE_R_PyTorch(controller.URM_train)
 easer.fit(10)
