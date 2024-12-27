84d3875ccd155272ee049394e607f8dfe0907a33
leolipa <leonardo.liparulo@mail.polimi.it>
2024-12-27 11:53:33 +0100

    another update of pytorch version

--- trainEase.py
+++ trainEase.py
@@ -5,9 +5,11 @@ from EASE__R_PyTorch import EASE_R_PyTorch
 from ModelController import ModelController
 import pandas as pd
 
+from ModelNames import ModelName
+
 controller = ModelController()
 
 easer = EASE_R_PyTorch(controller.URM_train)
-easer.fit(10)
+optuna = controller.optunizer(ModelName.EASE_R_Recommender)
 cc, _ = controller.evaluator_test.evaluateRecommender(easer)
 print(cc.loc[10]["MAP"])
