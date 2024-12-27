eb28e90bce5814dcdb299610d0a5b09864f1a93b
leolipa <leonardo.liparulo@mail.polimi.it>
2024-12-27 15:06:17 +0100

    training files

--- trainItemCFKNN.py
+++ trainItemCFKNN.py
@@ -1,5 +1,5 @@
 from ModelController import ModelController
 from ModelNames import ModelName
-
+print("ITEMCF")
 controller = ModelController()
 optuna = controller.optunizer(ModelName.ItemKNNCFRecommender)
--- trainuser.py
+++ trainuser.py
@@ -1,5 +1,5 @@
 from ModelController import ModelController
 from ModelNames import ModelName
-
+print("USER")
 controller = ModelController()
 optuna = controller.optunizer(ModelName.UserKNNCFRecommender)
