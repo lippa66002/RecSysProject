08a5bc931c3dd0fb7af8cc7b107b4c8241d42aac
leolipa <leonardo.liparulo@mail.polimi.it>
2024-12-27 12:09:55 +0100

    upload of train slim on urm

--- training/trainslim.py
+++ training/trainslim.py
@@ -1,3 +1,8 @@
+import os
+
+# Torna alla directory principale del progetto
+os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/..")
+
 from ModelController import ModelController
 from ModelNames import ModelName
 
