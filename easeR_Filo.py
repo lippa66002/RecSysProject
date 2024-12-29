f36b7e0395b51f12c51a62c3b23482da76a31f89
Filippo <filippo1.gentili@mail.polimi.it>
2024-12-29 16:54:12 +0100

    try to lower memory usage

--- easeR_Filo.py
+++ easeR_Filo.py
@@ -9,6 +9,9 @@ from Recommenders.BaseSimilarityMatrixRecommender import BaseItemSimilarityMatri
 from Recommenders.Recommender_utils import similarityMatrixTopK, check_matrix
 from Utils.seconds_to_biggest_unit import seconds_to_biggest_unit
 
+from torch.sparse import mm
+from torch.linalg import solve
+
 class EASE_R_Filo(BaseItemSimilarityMatrixRecommender):
     RECOMMENDER_NAME = "EASE_R_Filo"
 
@@ -21,49 +24,48 @@ class EASE_R_Filo(BaseItemSimilarityMatrixRecommender):
     def fit(self, topK=None, l2_norm=1e3, normalize_matrix=False):
 
         torch.cuda.empty_cache()
-
         start_time = time.time()
         self._print("Fitting model... ")
 
         if normalize_matrix:
-            # Normalize rows and columns
             self.URM_train = normalize(self.URM_train, norm='l2', axis=1)
             self.URM_train = normalize(self.URM_train, norm='l2', axis=0)
             self.URM_train = sps.csr_matrix(self.URM_train)
 
-        # Convert the matrix to PyTorch sparse tensor
+        # Convert to sparse tensor
         urm_train_torch = self._convert_sparse_matrix_to_torch(self.URM_train)
 
-        # Compute Gram matrix: X^T * X
-        gram_matrix = urm_train_torch.T @ urm_train_torch
-        gram_matrix = gram_matrix.to_dense()  # Convert to dense for inversion
+        # Compute sparse Gram matrix
+        gram_matrix = mm(urm_train_torch.T, urm_train_torch)
 
-        # Add l2 regularization to diagonal
+        # Add l2 regularization
         diag_indices = torch.arange(gram_matrix.size(0), device=self.device)
+        gram_matrix = gram_matrix.to_dense()
         gram_matrix[diag_indices, diag_indices] += l2_norm
 
-        # Compute P = (Gram Matrix)^-1
-        P = LA.inv(gram_matrix)
+        # Solve instead of invert
+        identity_matrix = torch.eye(gram_matrix.size(0), device=self.device, dtype=torch.float16)
+        P = solve(gram_matrix, identity_matrix)
 
         # Compute B matrix
         B = P / (-torch.diag(P).view(-1, 1))
         B[diag_indices, diag_indices] = 0.0
 
-        new_time_value, new_time_unit = seconds_to_biggest_unit(time.time() - start_time)
-        self._print("Fitting model... done in {:.2f} {}".format(new_time_value, new_time_unit))
-
-        # Convert B matrix to NumPy for sparse or dense storage
+        # Filter with topK
         B_np = B.cpu().numpy()
         if topK is not None:
             B_np = similarityMatrixTopK(B_np, k=topK, use_absolute_values=True, verbose=False)
 
         if self._is_content_sparse_check(B_np):
             self._print("Detected model matrix to be sparse, changing format.")
-            self.W_sparse = check_matrix(B_np, format='csr', dtype=np.float32)
+            self.W_sparse = check_matrix(B_np, format='csr', dtype=np.float16)
         else:
-            self.W_sparse = check_matrix(B_np, format='npy', dtype=np.float32)
+            self.W_sparse = check_matrix(B_np, format='npy', dtype=np.float16)
             self._W_sparse_format_checked = True
             self._compute_item_score = self._compute_score_W_dense
+
+        new_time_value, new_time_unit = seconds_to_biggest_unit(time.time() - start_time)
+        self._print("Fitting model... done in {:.2f} {}".format(new_time_value, new_time_unit))
 
     def _is_content_sparse_check(self, matrix):
         if self.sparse_threshold_quota is None:
@@ -73,8 +75,9 @@ class EASE_R_Filo(BaseItemSimilarityMatrixRecommender):
 
     def _convert_sparse_matrix_to_torch(self, sparse_matrix):
         coo = sparse_matrix.tocoo()
-        indices = torch.tensor([coo.row, coo.col], dtype=torch.long, device=self.device)
-        values = torch.tensor(coo.data, dtype=torch.float32, device=self.device)
+        indices = np.vstack((coo.row, coo.col))
+        indices = torch.tensor(indices, dtype=torch.long, device=self.device)
+        values = torch.tensor(coo.data, dtype=torch.float16, device=self.device)
         shape = torch.Size(coo.shape)
         return torch.sparse_coo_tensor(indices, values, shape)
 
@@ -83,7 +86,7 @@ class EASE_R_Filo(BaseItemSimilarityMatrixRecommender):
         user_profile_array = self.URM_train[user_id_array]
 
         if items_to_compute is not None:
-            item_scores = -np.ones((len(user_id_array), self.URM_train.shape[1]), dtype=np.float32) * np.inf
+            item_scores = -np.ones((len(user_id_array), self.URM_train.shape[1]), dtype=np.float16) * np.inf
             item_scores_all = user_profile_array.dot(self.W_sparse)
             item_scores[:, items_to_compute] = item_scores_all[:, items_to_compute]
         else:
