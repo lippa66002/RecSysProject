0d595a4bfc7002b2bda94973e0aca21f82338602
leolipa <leonardo.liparulo@mail.polimi.it>
2025-01-04 10:18:18 +0100

    training

---
+++ alphatrain.py
@@ -0,0 +1,6 @@
+from ModelController import ModelController
+from ModelNames import ModelName
+
+print("alpha")
+controller = ModelController()
+optuna = controller.optunizer(ModelName.P3alphaRecommender)
