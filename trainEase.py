5e854075656f0e568d4de858fe97867274db6ed1
leolipa <leonardo.liparulo@mail.polimi.it>
2025-01-04 09:44:28 +0100

    training

--- trainEase.py
+++ trainEase.py
@@ -19,9 +19,4 @@ ICM = pd.read_csv(filepath_or_buffer="Data/data_ICM_metadata.csv",
 URM_all, ICM_all = DataHandler.create_urm_icm(URM_all_dataframe, ICM)
 
 
-ease2 = EASE_R_Recommender(URM_all)
-ease2.fit(topK= 6, l2_norm= 12.941342785860702, normalize_matrix= False)
-ease2.save_model(folder_path="_saved_models", file_name="easeall2")
-ease2 = EASE_R_Recommender(controller.URM_train)
-ease2.fit(topK= 6, l2_norm= 12.941342785860702, normalize_matrix= False)
-ease2.save_model(folder_path="_saved_models", file_name="easeatrain2")
+optuna = controller.optunizer(ModelName.EASE_R_Recommender)
