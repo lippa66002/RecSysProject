acd039623945c713f7539843ca536271c3e71bc6
leolipa <leonardo.liparulo@mail.polimi.it>
2025-01-05 10:20:46 +0100

    training

--- savebest.py
+++ savebest.py
@@ -2,8 +2,6 @@ from ModelController import ModelController
 from Recommenders.SLIM.SLIMElasticNetRecommender import SLIMElasticNetRecommender
 import scipy.sparse as sps
 controller = ModelController()
-slim = SLIMElasticNetRecommender(controller.URM_train)
-stacked = sps.vstack([0.8718332784366307 * controller.URM_train, (1 - 0.8718332784366307) * controller.ICM_all.T]).tocsr()
-slim = SLIMElasticNetRecommender(stacked)
-slim.fit(alpha =7.558004074517556e-05, topK=641, l1_ratio= 0.08844974678228634)
-slim.save_model(folder_path="_saved_models", file_name="SLIMstackedAll2")
+slim = SLIMElasticNetRecommender(controller.URM_boost)
+slim.fit(alpha= 0.00022742003969239836, topK= 709, l1_ratio =0.1488442906776265)
+slim.save_model(folder_path="_saved_models", file_name="slimboost")
