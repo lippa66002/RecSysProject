7129090b867656c00b2b86159fa99d7957d7deca
leolipa <leonardo.liparulo@mail.polimi.it>
2025-01-05 22:39:19 +0100

    ss

--- trainkaggle2.py
+++ trainkaggle2.py
@@ -33,21 +33,21 @@ item.fit(topK= 9, shrink= 956, similarity= 'cosine', normalize= True, feature_we
 
 
 def objective_function_scores_hybrid_1( optuna_trial):
-    print("itemcf + slim")
+    print("user + slim +rp3")
 
     # bpr = SLIM_BPR_Cython(self.URM_train)
     # bpr.load_model(folder_path="_saved_models", file_name="SLIM_BPR_Recommender_train")
-    recom1 = ScoresHybridRecommender(controller.URM_train, itemmm, slim4, bestrp3, slim4, slim4)
+    recom1 = ScoresHybridRecommender(controller.URM_train, user, slim4, bestrp3, slim4, slim4)
 
     alpha = optuna_trial.suggest_float("alpha", 0.0, 1.0)
 
+    beta = 0.006228402274954442
 
 
 
 
 
-
-    recom1.fit(alpha, 1-alpha, 0, 0, 0)
+    recom1.fit(beta, 1-beta , alpha, 0, 0)
 
     result_df, _ = controller.evaluator_test.evaluateRecommender(recom1)
     return result_df.loc[10]["MAP"]
