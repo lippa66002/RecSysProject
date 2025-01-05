c2f40c09ba0edc0b37225fe45f78331098f31766
leolipa <leonardo.liparulo@mail.polimi.it>
2025-01-05 20:10:29 +0100

    Aggiunto il file trainkaggle.py

---
+++ trainkaggle.py
@@ -0,0 +1,59 @@
+from ModelController import ModelController
+from Optimize.SaveResults import SaveResults
+from Recommenders.GraphBased.RP3betaRecommender import RP3betaRecommender
+import scipy.sparse as sps
+import optuna
+from Recommenders.KNN.ItemKNNCBFRecommender import ItemKNNCBFRecommender
+from Recommenders.KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
+from Recommenders.KNN.UserKNNCFRecommender import UserKNNCFRecommender
+from Recommenders.NonPersonalizedRecommender import TopPop
+from Recommenders.SLIM.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
+from Recommenders.SLIM.SLIMElasticNetRecommender import SLIMElasticNetRecommender
+from Recommenders.ScoresHybridRecommender import ScoresHybridRecommender
+
+controller = ModelController()
+bestrp3 = RP3betaRecommender(controller.URM_train)
+bestrp3.fit(topK= 18, beta= 0.2449115248846201, alpha= 0.34381573319072084)
+stacked = sps.vstack([0.8392863849420211 * controller.URM_train, (1 - 0.8392863849420211) * controller.ICM_all.T]).tocsr()
+slim4 = SLIMElasticNetRecommender(stacked)
+slim4.load_model(folder_path="_saved_models",file_name="SLIMstackedTrain3")
+itemmm = ItemKNNCFRecommender(controller.URM_train)
+itemmm.fit(similarity= 'tversky', topK= 5, shrink= 15, tversky_alpha= 0.0291003114865242, tversky_beta= 1.0501107741561788)
+user = UserKNNCFRecommender(controller.URM_train)
+user.fit(topK= 1000, shrink= 16, similarity ='cosine', normalize= True, feature_weighting= 'BM25')
+slimbpr = SLIM_BPR_Cython(controller.URM_train)
+slimbpr.fit( topK= 20, learning_rate= 0.07235759859199255, lambda_i= 0.0026131161353345695, lambda_j= 0.0025413226707704894, symmetric= True, sgd_mode= 'adagrad'
+)
+top_pop = TopPop(controller.URM_boost)
+top_pop.fit()
+item = ItemKNNCBFRecommender(controller.URM_train, controller.ICM_all)
+item.fit(topK= 9, shrink= 956, similarity= 'cosine', normalize= True, feature_weighting= 'BM25')
+
+
+def objective_function_scores_hybrid_1( optuna_trial):
+
+    # bpr = SLIM_BPR_Cython(self.URM_train)
+    # bpr.load_model(folder_path="_saved_models", file_name="SLIM_BPR_Recommender_train")
+    recom1 = ScoresHybridRecommender(controller.URM_train, user, slim4, bestrp3, slim4, slim4)
+
+    alpha = optuna_trial.suggest_float("alpha", 0.0, 100.0)
+    beta = optuna_trial.suggest_float("beta", 0.0, 100.0)
+    gamma = optuna_trial.suggest_float("gamma", 0.0, 100.0)
+    print(1)
+
+
+
+
+
+
+    recom1.fit(alpha, beta, gamma, 0, 0)
+
+    result_df, _ = controller.evaluator_test.evaluateRecommender(recom1)
+    return result_df.loc[10]["MAP"]
+optuna_study = optuna.create_study(direction="maximize")
+save_results = SaveResults()
+optuna_study.optimize(objective_function_scores_hybrid_1,
+                              callbacks=[save_results],
+                              n_trials=200)
+print(save_results.results_df)
+print(optuna_study.best_trial.params)
