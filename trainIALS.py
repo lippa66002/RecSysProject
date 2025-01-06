92e8121e442ede21ff3b9b4848045cd67b8ed20e
lippa66002 <160721155+lippa66002@users.noreply.github.com>
2025-01-06 13:04:22 +0100

    ss

--- trainIALS.py
+++ trainIALS.py
@@ -8,4 +8,4 @@ controller = ModelController()
 
 ials = IALSRecommender(controller.URM_train)
 ials.fit(confidence_scaling= 'linear', num_factors=173, alpha= 6.7979092186218715, epsilon= 1.3000803763047268, reg=0.03413944416805373, init_std= 0.06581036009302164)
-ials.save_model(folder_path="_saved_models", file_name="IALS_all")
+ials.save_model(folder_path="_saved_models", file_name="IALS_train")
