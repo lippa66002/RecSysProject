21fb3041c8d33e2fa4919bca3436f7dd1c3239d8
lippa66002 <160721155+lippa66002@users.noreply.github.com>
2025-01-06 17:35:13 +0100

    ss

--- trainmultvae.py
+++ trainmultvae.py
@@ -20,11 +20,11 @@ def objective_function_multVAE(optuna_trial):
     recommender_instance = MultVAERecommender(controller.URM_train)
     full_hyperp = {
         # "epochs": optuna_trial.suggest_int("epochs", 10, 100),
-        "learning_rate": optuna_trial.suggest_float("learning_rate", 1e-4, 1e-2, log=True),
-        "batch_size": optuna_trial.suggest_int("batch_size", 32, 512),
-        "dropout": optuna_trial.suggest_float("dropout", 0.1, 0.5),
-        "anneal_cap": optuna_trial.suggest_float("anneal_cap", 0.1, 0.5),
-        "epochs" : optuna_trial.suggest_int("epochs" ,50,300)
+        "learning_rate": optuna_trial.suggest_float("learning_rate", 1e-7, 1, log=True),
+        "batch_size": optuna_trial.suggest_int("batch_size", 1, 2512),
+        "dropout": optuna_trial.suggest_float("dropout", 0.1, 0.9),
+        "anneal_cap": optuna_trial.suggest_float("anneal_cap", 0.1, 2),
+        "epochs" : optuna_trial.suggest_int("epochs" ,50,1000)
     }
     recommender_instance.fit(**full_hyperp)
     result_df, _ = controller.evaluator_test.evaluateRecommender(recommender_instance)
