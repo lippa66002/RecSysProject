d898d0143244516c8f1f12a96b0618aa01d46c09
FilippoGentili <filippo1.gentili@mail.polimi.it>
2025-01-07 19:04:11 +0100

    train

--- RecommendOnAllSet.py
+++ RecommendOnAllSet.py
@@ -5,6 +5,7 @@ from ModelController import ModelController
 from Recommenders.EASE_R.EASE_R_Recommender import EASE_R_Recommender
 from Recommenders.GraphBased.P3alphaRecommender import P3alphaRecommender
 from Recommenders.GraphBased.RP3betaRecommender import RP3betaRecommender
+from Recommenders.HybridOptunable2 import HybridOptunable2
 from Recommenders.KNN.UserKNNCFRecommender import UserKNNCFRecommender
 from Recommenders.SLIM.SLIMElasticNetRecommender import SLIMElasticNetRecommender
 import scipy.sparse as sps
@@ -64,43 +65,67 @@ model2 = SLIMElasticNetRecommender(controller.URM_train)
 model1.load_model(folder_path="_saved_models", file_name="RP3betaRecommender")
 model2.load_model(folder_path="_saved_models", file_name="SLIMElasticNetRecommender")
 """
-stacked = sps.vstack([0.8392863849420211 * URM_all, (1 - 0.8392863849420211) * controller.ICM_all.T]).tocsr()
-slim = SLIMElasticNetRecommender(URM_all)
-slim.load_model(folder_path="_saved_models", file_name="SLIM_ElasticNetAll")
-#slim.fit(alpha= 0.00022742003969239836, topK= 709, l1_ratio= 0.1488442906776265)
+stacked = sps.vstack([0.8392863849420211 * controller.URM_train, (1 - 0.8392863849420211) * controller.ICM_all.T]).tocsr()
+stacked2 = sps.vstack([0.6814451172353111 * controller.URM_train, (1 - 0.6814451172353111) * controller.ICM_all.T]).tocsr()
 
-ease = EASE_R_Recommender(URM_all)
-ease.load_model(folder_path="_saved_models", file_name="easeall")
+
+slim_t = SLIMElasticNetRecommender(controller.URM_train)
+slim_t.load_model(folder_path="_saved_models", file_name="SLIMtrain")
+
+slim_s = SLIMElasticNetRecommender(stacked2)
+slim_s.load_model(folder_path="_saved_models", file_name="SLIMstackedTrainval1")
+
+#bpr = SLIM_BPR_Cython(controller.URM_train)
+#bpr.fit(topK =  11, learning_rate =  0.04193849345153912, lambda_i=  0.009876208709609856, lambda_j= 0.00044296738036044263, symmetric =  True, sgd_mode =  'adagrad')
+
+#item = ItemKNNCFRecommender(controller.URM_train)
+#item.fit(similarity =  "cosine", topK =  8, shrink= 12)
 
 rp3 = RP3betaRecommender(stacked)
 rp3.load_model(folder_path="_saved_models", file_name="rp3_stacked3_f")
-#rp3.fit(topK= 21, beta= 0.2263343041398906, alpha= 0.47403955777118195)
+#rp3.fit(topK= 18, beta= 0.2449115248846201, alpha= 0.34381573319072084)
+#rp3.save_model(folder_path="_saved_models", file_name="rp3_train_f")
 
-#hyb = HybridOptunable2(URM_all)
-#hyb.fit(0.24002684672441646, slim, rp3)
+easeR = EASE_R_Recommender(controller.URM_train)
+easeR.load_model(folder_path="_saved_models", file_name="easetrainll")
+#easeR.fit(topK= 32, l2_norm= 20.402285200199643, normalize_matrix= False)
+#slim.save_model(folder_path="_saved_models", file_name="ease_train_f")
 
-user = UserKNNCFRecommender(URM_all)
+p3 = P3alphaRecommender(controller.URM_train)
+p3.load_model(folder_path="_saved_models", file_name="p3alpha_train_f")
+
+user = UserKNNCFRecommender(controller.URM_train)
 user.fit(topK= 995, shrink= 398, similarity= 'cosine', normalize= True, feature_weighting= 'BM25')
 
-p3 = P3alphaRecommender(URM_all)
-p3.load_model(folder_path="_saved_models", file_name="p3alpha_all_f")
+#user.load_model(folder_path="_saved_models", file_name="user_train_f")
+
+#items = ItemKNNCBFRecommender(controller.URM_train, controller.ICM_all)
+#items.fit(topK= 6, shrink= 693, similarity= 'cosine', normalize= True, feature_weighting= 'BM25')
+
+# hyb = ItemKNN_CFCBF_Hybrid_Recommender(controller.URM_train, controller.ICM_all)
+# hyb.fit(topK =  6, shrink =  167, similarity =  'asymmetric', normalize =  False, feature_weighting =  'BM25', ICM_weight =  0.375006792830105)
+
+hyb_slims = HybridOptunable2(controller.URM_train)
+hyb_slims.fit(0.27959722573911727,slim_t,slim_s)
+
+bestrp3 = RP3betaRecommender(controller.URM_train)
+bestrp3.load_model(folder_path="_saved_models", file_name="rp3train")
+
+hyb_best = HybridOptunable2(controller.URM_train)
+hyb_best.fit(0.18923840370620948,hyb_slims,bestrp3)
 
 #alpha=0.9947414494756955
-recommender_instance = ScoresHybridRecommender(URM_all, slim, rp3, ease, user, p3)
+recommender_instance = ScoresHybridRecommender(URM_all, hyb_best, user, p3, user, p3)
 
 # Sample x, y, and z to calculate weights
-x = 0.6709569697381116
-y = 0.9407851726464864
-z = 0.668214489158288
-w = 0.8401488658427435
+x = 0.9809789503691551
+y = 0.3078230973689968
 
 alpha = x
 beta = y * (1 - x)
-gamma = z * (1 - x) * (1 - y)
-delta = w * (1 - x) * (1 - y) * (1 - z)
-epsilon = (1 - x) * (1 - y) * (1 - z) * (1 - w)
+gamma = (1 - x) * (1 - y)
 
-recommender_instance.fit(alpha, beta, gamma, delta, epsilon)
+recommender_instance.fit(alpha, beta, gamma, 0, 0)
 
 
 result_df, _ = controller.evaluator_test.evaluateRecommender(recommender_instance)
