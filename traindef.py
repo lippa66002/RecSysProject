18be71262a5db3d10d133e32d2882fe106fa8c62
lippa66002 <160721155+lippa66002@users.noreply.github.com>
2025-01-07 09:15:57 +0100

    ss

--- traindef.py
+++ traindef.py
@@ -16,27 +16,26 @@ slim1 = SLIMElasticNetRecommender(stacked)
 slim1.load_model(folder_path="_saved_models", file_name="SLIMstackedTrainval1")
 slim2 = SLIMElasticNetRecommender(controller.URM_train)
 slim2.load_model(folder_path="_saved_models", file_name="SLIMtrain")  #0.3588980025585651 slim1slim2 score,  0.7228086650480543 slim1slim2 optunable2
+rp3 = RP3betaRecommender(controller.URM_train)
 bestrp3 = RP3betaRecommender(controller.URM_train)
 bestrp3.fit(topK= 18, beta= 0.2449115248846201, alpha= 0.34381573319072084)
-hyb1 = HybridOptunable2(controller.URM_train)
-hyb1.fit(0.7228086650480543,slim1,slim2)
 
 def objective_function_scores_hybrid_1( optuna_trial):
 
     # bpr = SLIM_BPR_Cython(self.URM_train)
     # bpr.load_model(folder_path="_saved_models", file_name="SLIM_BPR_Recommender_train")
-    print("hyb1+rp3 score")
-    recom1 = ScoresHybridRecommender(controller.URM_train, hyb1, bestrp3, bestrp3, slim1, slim1)
+    print("slim1+slim2+rp3 score con valori fissi slim1slim2")
+    recom1 = ScoresHybridRecommender(controller.URM_train, slim1, slim2, rp3, slim1, slim1)
 
-    alpha = optuna_trial.suggest_float("alpha", 0.0, 1.0)
-
-
+    gamma = optuna_trial.suggest_float("gamma", 0.0, 1.0)
 
 
 
 
 
-    recom1.fit(alpha, 1-alpha, 0, 0, 0)
+
+
+    recom1.fit(0.3588980025585651, 1-0.3588980025585651, gamma, 0, 0)
 
     result_df, _ = controller.evaluator_test.evaluateRecommender(recom1)
     return result_df.loc[10]["MAP"]
@@ -48,16 +47,15 @@ optuna_study.optimize(objective_function_scores_hybrid_1,
 print(save_results.results_df)
 print(optuna_study.best_trial.params)
 
-'''
-def objective_function_scores_hybrid_6( optuna_trial):
+
+'''def objective_function_scores_hybrid_6( optuna_trial):
     # bpr = SLIM_BPR_Cython(self.URM_train)
     # bpr.load_model(folder_path="_saved_models", file_name="SLIM_BPR_Recommender_train")
-    print("hyb1+rp3 hybridoptunable2")
     recom1 = HybridOptunable2(controller.URM_train)
 
     alpha = optuna_trial.suggest_float("alpha", 0, 1)
 
-    recom1.fit(alpha,hyb1,bestrp3)
+    recom1.fit(alpha,slim1,slim2)
 
     result_df, _ = controller.evaluator_test.evaluateRecommender(recom1)
     return result_df.loc[10]["MAP"]
